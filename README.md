c26d1c1f53dc930de169f0b8b6482873dc81949a
t73liu <t73liu@gmail.com>
2018-09-03 14:52:44 -0400

    Implement basic fs storage

--- README.md
+++ README.md
@@ -4,8 +4,25 @@ Persist redux store to filesystem because of the following android limitation [#
 
 Existing filesystem for redux-persist require ejecting of CRNA/Expo apps due to react-native linking.
 
+Comes with typescript definitions.
+
 ## Setup
 
-1. yarn add redux-persist-expo-filesystem
+1. Install: yarn add redux-persist-expo-filesystem
 1. Make sure expo is a peer dependency
-1. ...
+1. Usage below:
+
+```javascript
+import ExpoFileSystemStorage from "redux-persist-expo-filesystem"
+const persistConfig = {
+  key: "root",
+  storage: ExpoFileSystemStorage
+};
+const store = createStore(
+  persistReducer(persistConfig, rootReducer)
+);
+```
+
+## Compatibility
+
+Confirmed with expo 29.0.0 and redux-persist 5.10.0
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 import {FileSystem} from "expo";
+
+function generateFolderPath() {
+    return FileSystem.documentDirectory + "appData/";
+}
+
+function generateFilePath(key) {
+    const fileName = key.replace(/[^a-z0-9.\-_]/gi, "-");
+    return generateFolderPath() + fileName;
+}
+
+function writeFile(path, value) {
+    FileSystem.writeAsStringAsync(generateFilePath(key), value);
+}
+
+const ExpoFileSystemStorage = {
+    getItem(key) {
+        return FileSystem.readAsStringAsync(generateFilePath(key));
+    },
+    setItem(key, value) {
+        const folderPath = generateFolderPath();
+        return FileSystem.getInfoAsync(folderPath)
+            .then(info => {
+                const filePath = generateFilePath(key);
+                if (info.exists) {
+                    writeFile(filePath, value);
+                } else {
+                    FileSystem.makeDirectoryAsync(folderPath, {intermediates: true})
+                        .then(() => writeFile(filePath, value));
+                }
+            });
+    },
+    removeItem(key) {
+        return FileSystem.deleteAsync(generateFilePath(key), {idempotent: true});
+    }
+};
+
+export default ExpoFileSystemStorage;
